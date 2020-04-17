639bdd1df91fde227a3806fb129f7dd5b0504c4a
Jimmy <jrsutherland78@googlemail.com>
2020-04-17 16:30:46 +0100

    add z index to model

--- src/app/models/screen-display.model.ts
+++ src/app/models/screen-display.model.ts
@@ -3,5 +3,6 @@ import { ImageDetailModel } from '../models/image-detail.model';
 export class ScreenDisplayModel extends ImageDetailModel{
   x: number;
   y: number;
+  z: number;
   size: number;
 }
