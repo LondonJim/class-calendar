058afe119d5466a61414a8053c0689d1b4a19cb6
Jimmy <jrsutherland78@googlemail.com>
2020-04-17 16:34:40 +0100

    add days model

---
+++ src/app/models/days.model.ts
@@ -0,0 +1,9 @@
+import { ScreenDisplayModel } from '../models/screen-display.model';
+
+export class DaysModel {
+  monday: ScreenDisplayModel;
+  tuesday: ScreenDisplayModel;
+  wednesday: ScreenDisplayModel;
+  thursday: ScreenDisplayModel;
+  friday: ScreenDisplayModel;
+}
