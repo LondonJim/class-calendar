0e0fa72db3985d985de91c09c6c183d9d0fb867d
Jimmy <jrsutherland78@googlemail.com>
2021-09-12 19:09:15 +0100

    change path

--- server.js
+++ server.js
@@ -12,5 +12,9 @@ app.get('*', (req, res) => {
   res.sendFile(`./dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
 
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
