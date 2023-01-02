8ccbed7ba6c431dc9530964718117dd6dc3d0caf
Jimmy <jrsutherland78@googlemail.com>
2023-01-02 12:47:50 +0000

    update to angular 11

--- src/app/main/drawing-canvas/drawing-canvas.component.spec.ts
+++ src/app/main/drawing-canvas/drawing-canvas.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DrawingCanvasComponent } from './drawing-canvas.component';
 
@@ -6,7 +6,7 @@ describe('DrawingCanvasComponent', () => {
   let component: DrawingCanvasComponent;
   let fixture: ComponentFixture<DrawingCanvasComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DrawingCanvasComponent ]
     })
